1073db6a4bd39331ec1fe910c1deaac520e0449c
3Omar3 <emmanuelomarrr@hotmail.com>
2017-07-13 01:02:43 -0500

    Update main.cpp
    Nuevos Comentarios, faltan agregar mas aun

--- Ejercicio0638/main.cpp
+++ Ejercicio0638/main.cpp
@@ -3,13 +3,20 @@
 #include <iostream>
 using namespace std;
 
+/* REGLAS:
+1- Solo se puede mover un disco a la vez.
+2- No se puede colocar un disco mas grande sobre uno mas pequenio.
+3- Todos los discos deben de llegar a la tercera Torre .
+*/
+
 inline const void torreHanoi( unsigned int torre1, unsigned int torre2, unsigned int torre3, unsigned int Discos )
 {
-    if ( Discos == 1 )
+    if ( Discos == 1 ) // Si solo hay un disco se movera a la tercera torre.
         cout << torre1 << " --> " << torre3 << endl;
         else{
+            // Se cambia la pocision ya que nuestra torre auxiliar ya esta ocupada
             torreHanoi( torre1, torre3, torre2, Discos - 1 );
-            cout << torre1 << " --> " << torre3 << endl;
+            cout << torre1 << " --> " << torre3 << endl; // se cambian los valores cada vez despues del ultimo ciclo
             torreHanoi( torre2, torre1, torre3, Discos - 1 );
         }
 }
@@ -17,13 +24,15 @@ inline const void torreHanoi( unsigned int torre1, unsigned int torre2, unsigned
 int main()
 {
     cout << "!!TORRE DE HANOI!!\n" << endl;
-    // Se definen las Variables 
+    
+    // Se definen las Variables, Ademas las torres deben de tener la posicion que van a ocupar en el juego.
+    // La segunda torre sera como nuestro apoyo, "auxiliar" para almacenar los discos.
     unsigned int torre1 = 1, torre2 = 2, torre3 = 3, Discos = 0;
     
     cout << "Numero de Discos: ";
     cin >> Discos;
-    
+    // Se llama a la funcion
     torreHanoi( torre1, torre2, torre3, Discos );
     
-    return 0;
+    return 0; // :3
 }
